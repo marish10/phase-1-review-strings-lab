b9e9f73f79d845c7704022fab6f23492c6414d06
Maryan10 <maryamali@gmail.com>
2025-08-13 09:37:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// 1) Define currentUser
+const currentUser = 'Grace Hopper';
+
+// 2–4) Create welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 5–7) Create excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// 8–10) Create shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
